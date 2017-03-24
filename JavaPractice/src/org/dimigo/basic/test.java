f64c463f38613bf091dd4fab219f2307f748d282
soon3 <soon3@DESKTOP-MQ0P89A>
2017-03-24 19:30:26 +0900

    test

---
+++ JavaPractice/src/org/dimigo/basic/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_ test
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 24.
+ * </pre>
+ * 
+ * @author : soon3
+ * @version : 1.0
+ */
+public class test {
+
+}
