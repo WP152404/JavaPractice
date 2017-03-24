0ed02354b66f94e3474bc3a8a9879e070e2c8169
soon3 <soon3@DESKTOP-MQ0P89A>
2017-03-24 19:33:01 +0900

    test

--- JavaPractice/src/org/dimigo/basic/Loop.java
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -65,13 +65,7 @@ public class Loop {
 			
 		case 9:
 			System.out.println("이제 공부하세요!");
-			break;
-		default:
-			System.out.println("없는 메뉴입니다!!");
-			break;
 		}
-		
-			
 		
 		}while(choice!=9);
 	}
